8acf9e3c46a9befd74df2eca686725782778a1d1
Deepak-prog123 <deepakjuya07@gmail.com>
2025-09-03 16:18:41 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Wee Wonderland",
-  description: "",
+  description: "Wee Wonderland",
 };
 
 export default function RootLayout({
